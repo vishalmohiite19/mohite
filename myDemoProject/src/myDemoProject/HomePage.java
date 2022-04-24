765a7e1c4866fe4ca7decb9a800b5ef627034f76
vishal <vmohite60@gmail.com>
2022-04-24 14:56:59 +0530

    3rd commit
    3rd commit

---
+++ myDemoProject/src/myDemoProject/HomePage.java
@@ -0,0 +1,15 @@
+package myDemoProject;
+
+public class HomePage {
+
+	
+		public static void main(String[] args) {
+			System.out.println("hello i am vishal mohite");
+			System.out.println(" i am from aurangabad");
+			System.out.println("9372255442");
+
+
+
+	}
+
+}
---
+++ myDemoProject/src/myDemoProject/ProductPage.java
@@ -0,0 +1,11 @@
+package myDemoProject;
+
+public class ProductPage {
+
+	public static void main(String[] args) {
+		System.out.println("how are you");
+
+
+	}
+
+}
